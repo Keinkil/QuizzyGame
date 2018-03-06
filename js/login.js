f0b8650b19844632656b1e82f7e4fab890d59555
Vecochan <vedrana.zeba@gmail.com>
2018-03-06 23:08:28 +0100

    login stuff
    fake login. Lagt till mappstruktur för css, image, javascript

---
+++ js/login.js
@@ -0,0 +1,40 @@
+$(function(){
+  var passwordfield = $("input[name=password]");
+var textfield = $("input[name=user]");
+            $('button[type="submit"]').click(function(e) {
+                e.preventDefault();
+                //little validation just to check username
+                if (textfield.val() != "" && passwordfield.val() != "") {
+                    //$("body").scrollTo("#output");
+                    $("#output").addClass("alert alert-success animated fadeInUp").html("Welcome back " + "<span style='text-transform:uppercase'>" + textfield.val() + "</span>");
+                    $("#output").removeClass(' alert-danger');
+                    $("input").css({
+                    "height":"0",
+                    "padding":"0",
+                    "margin":"0",
+                    "opacity":"0"
+                    });
+
+                    //change button text
+                    $('button[type="submit"]').html("continue")
+                    .removeClass("btn-info")
+                    .addClass("btn-default").click(function(){
+                    $("input").css({
+                    "height":"auto",
+                    "padding":"10px",
+                    "opacity":"1"
+                    }).val("");
+                    });
+
+                    $('button[type="submit"]').click(function(){
+                      window.location.replace("web.html")
+                    });
+                } else {
+                    //remove success mesage replaced with error message
+                    $("#output").removeClass(' alert alert-success');
+                    $("#output").addClass("alert alert-danger animated fadeInUp").html("sorry enter a username and password");
+                }
+                //console.log(textfield.val());
+
+            });
+});
